02cff8dc23e4573bd8a5d8af433d0dce3ed633b6
Rob Rix <rob.rix@github.com>
2014-04-12 22:57:10 -0400

    Define a block type

--- Obstruct/Obstruct.h
+++ Obstruct/Obstruct.h
@@ -1,13 +1,6 @@
-//
-//  Obstruct.h
-//  Obstruct
-//
-//  Created by Rob Rix on 2014-04-12.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
-#import <Foundation/Foundation.h>
+#include <Block.h>
 
-@interface Obstruct : NSObject
+typedef struct obstr_block_s *obstr_block_t;
 
-@end
