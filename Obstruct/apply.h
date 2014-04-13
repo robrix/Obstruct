886c56cc41352a0e1a11c4d754989cbf8eeed056
Rob Rix <rob.rix@github.com>
2014-04-13 02:03:29 -0400

    Add and test applying arrays.

--- Obstruct/apply.h
+++ Obstruct/apply.h
@@ -7,4 +7,14 @@
 
 intptr_t obstruct_apply(obstr_block_t block, uintptr_t count, intptr_t arguments[count]);
 
+#ifdef __OBJC__
+
+static id obstruct_apply_array(id(^block)(), NSArray *array) {
+	__unsafe_unretained id objects[array.count];
+	[array getObjects:objects range:(NSRange){ .length = array.count }];
+	return (__bridge id)(void *)obstruct_apply((__bridge obstr_block_t)block, array.count, (intptr_t *)(void *)objects);
+}
+
+#endif
+
 #endif
--- ObstructTests/ObstructTests.m
+++ ObstructTests/ObstructTests.m
@@ -2,6 +2,7 @@
 
 #import <XCTest/XCTest.h>
 #import <Obstruct/Obstruct.h>
+#import <Obstruct/apply.h>
 
 @interface ObstructTests : XCTestCase
 @end
@@ -10,6 +11,11 @@
 
 -(void)testBlockArity {
 	XCTAssertEqual(obstr_block_get_arity((__bridge obstr_block_t)^ NSString * (id _0, id<NSCopying> _1, NSString<NSCopying> *_2){ return @""; }), 3, @"");
+}
+
+-(void)testApply {
+	XCTAssertEqualObjects(obstruct_apply_array(^(id x){ return x; }, @[ self ]), self, @"");
+	XCTAssertEqualObjects(obstruct_apply_array(^(NSNumber *x, NSNumber *y){ return @(x.integerValue + y.integerValue); }, @[ @12, @-5 ]), @7, @"");
 }
 
 @end
