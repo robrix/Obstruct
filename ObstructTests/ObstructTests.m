650bc57feb434e1c1c34c5826a902cab3bd4df94
Rob Rix <rob.rix@github.com>
2014-04-13 00:05:40 -0400

    Test block arity as scaffolding.

--- ObstructTests/ObstructTests.m
+++ ObstructTests/ObstructTests.m
@@ -1,34 +1,15 @@
-//
-//  ObstructTests.m
-//  ObstructTests
-//
-//  Created by Rob Rix on 2014-04-12.
 //  Copyright (c) 2014 Rob Rix. All rights reserved.
-//
 
 #import <XCTest/XCTest.h>
+#import <Obstruct/Obstruct.h>
 
 @interface ObstructTests : XCTestCase
-
 @end
 
 @implementation ObstructTests
 
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+-(void)testBlockArity {
+	XCTAssertEqual(obstr_block_get_arity((__bridge obstr_block_t)^ NSString * (id _0, id<NSCopying> _1, NSString<NSCopying> *_2){ return @""; }), 3, @"");
 }
 
 @end
