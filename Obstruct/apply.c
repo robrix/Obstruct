c997889a0773909a627f42e64c74fb7f4d4557ee
Rob Rix <rob.rix@github.com>
2014-04-13 01:44:33 -0400

    Apply.

---
+++ Obstruct/apply.c
@@ -0,0 +1,27 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#include "apply.h"
+#include <assert.h>
+
+intptr_t obstruct_apply(obstr_block_t block, uintptr_t count, intptr_t a[count]) {
+	typedef intptr_t t;
+	switch (obstr_block_get_arity(block)) {
+		case 0: return ((t(^)(void))block)();
+		case 1: return ((t(^)(t))block)(a[0]);
+		case 2: return ((t(^)(t,t))block)(a[0], a[1]);
+		case 3: return ((t(^)(t,t,t))block)(a[0], a[1], a[2]);
+		case 4: return ((t(^)(t,t,t,t))block)(a[0], a[1], a[2], a[3]);
+		case 5: return ((t(^)(t,t,t,t,t))block)(a[0], a[1], a[2], a[3], a[4]);
+		case 6: return ((t(^)(t,t,t,t,t,t))block)(a[0], a[1], a[2], a[3], a[4], a[5]);
+		case 7: return ((t(^)(t,t,t,t,t,t,t))block)(a[0], a[1], a[2], a[3], a[4], a[5], a[6]);
+		case 8: return ((t(^)(t,t,t,t,t,t,t,t))block)(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]);
+		case 9: return ((t(^)(t,t,t,t,t,t,t,t,t))block)(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]);
+		case 10: return ((t(^)(t,t,t,t,t,t,t,t,t,t))block)(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9]);
+		
+		default:
+			assert(0 == "block arity must be in interval 0..10");
+			break;
+	}
+	
+	return 0;
+}
---
+++ Obstruct/apply.h
@@ -0,0 +1,10 @@
+//  Copyright (c) 2014 Rob Rix. All rights reserved.
+
+#ifndef OBSTRUCT_APPLY
+#define OBSTRUCT_APPLY
+
+#include <Obstruct/Obstruct.h>
+
+intptr_t obstruct_apply(obstr_block_t block, uintptr_t count, intptr_t arguments[count]);
+
+#endif
