b751084988c291ac4cc9bde6b6cf126b667d3194
Rob Rix <rob.rix@github.com>
2014-04-18 01:16:29 -0400

    Correct the tests.

--- ObstructTests/ObstructTests.m
+++ ObstructTests/ObstructTests.m
@@ -10,12 +10,12 @@
 @implementation ObstructTests
 
 -(void)testBlockArity {
-	XCTAssertEqual(obstr_block_get_arity((__bridge obstr_block_t)^ NSString * (id _0, id<NSCopying> _1, NSString<NSCopying> *_2){ return @""; }), 3, @"");
+	XCTAssertEqual(obstr_block_get_arity((obstr_block_t)^(id _0, id<NSCopying> _1, NSString<NSCopying> *_2){ return @""; }), 3, @"");
 }
 
 -(void)testApply {
-	XCTAssertEqualObjects(obstruct_apply_array(^(id x){ return x; }, @[ self ]), self, @"");
-	XCTAssertEqualObjects(obstruct_apply_array(^(NSNumber *x, NSNumber *y){ return @(x.integerValue + y.integerValue); }, @[ @12, @-5 ]), @7, @"");
+	XCTAssertEqualObjects((obstr_block_apply_array(^(id x){ return x; }, @[ self ])), self, @"");
+	XCTAssertEqualObjects((obstr_block_apply_array(^(NSNumber *x, NSNumber *y){ return @(x.integerValue + y.integerValue); }, (@[ @12, @-5 ]))), @7, @"");
 }
 
 @end
