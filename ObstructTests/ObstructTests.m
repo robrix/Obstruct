1be5dd550a605029b4aa45f5f796178829fcb8f7
Rob Rix <rob.rix@github.com>
2014-04-18 01:16:38 -0400

    Test higher-order block arity.

--- ObstructTests/ObstructTests.m
+++ ObstructTests/ObstructTests.m
@@ -11,6 +11,8 @@
 
 -(void)testBlockArity {
 	XCTAssertEqual(obstr_block_get_arity((obstr_block_t)^(id _0, id<NSCopying> _1, NSString<NSCopying> *_2){ return @""; }), 3, @"");
+	obstr_block_t block = (obstr_block_t)^(dispatch_block_t(^_)(NSDictionary *,NSString<NSCoding> *, dispatch_block_t)){ return _; };
+	XCTAssertEqual(obstr_block_get_arity(block), 1, @"");
 }
 
 -(void)testApply {
